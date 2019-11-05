84e213aae1d8067c2802ffa890e93206a7feb3c2
Adam Stover <nehtaro@gmail.com>
2019-11-05 10:52:33 -0800

    updated package.json scripts and initializing reducer/actions

--- client/actions/actions.js
+++ client/actions/actions.js
@@ -1,2 +1,13 @@
 import * as types from '../constants/actionTypes';
 
+export const startNewGame = () => dispatch => {
+  fetch('/questions')
+    .then(res => res.json())
+    .then(data => {
+      dispatch({
+        type: types.START_NEW_GAME,
+        payload: data,
+      });
+    })
+    .catch(console.error);
+};
--- client/reducers/gameReducer.js
+++ client/reducers/gameReducer.js
@@ -9,11 +9,22 @@ const initialState = {
     isActive: false,
     activeQuestionIndex: 0,
     numCorrectAnswers: 0,
+    cards: [],
   },
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case types.START_NEW_GAME:
+      return {
+        ...state,
+        game: {
+          isActive: true,
+          activeQuestionIndex: 0,
+          numCorrectAnswers: 0,
+          cards: payload,
+        },
+      };
     default:
       return state;
   }
