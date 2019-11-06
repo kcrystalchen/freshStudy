c2717cec08e7e0849e4ed3f4f2777297667c10f1
Adam Stover <nehtaro@gmail.com>
2019-11-05 21:48:49 -0800

    .

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -11,7 +11,7 @@ export default ({
     <label>Username</label>
     <input type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)} required />
     <label>Password</label>
-    <input type="text" name="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+    <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
     <button type="submit" onClick={() => login(username, password)}>Login here!</button>
     <button type="button" onClick={handleToggle}>Create Account Here!</button>
     </div>
