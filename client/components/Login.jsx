7dd15190292ce77400a8db32bb6b3977d9fbda32
Adam Stover <nehtaro@gmail.com>
2019-11-05 21:46:23 -0800

    Login

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -3,13 +3,17 @@ import React from 'react';
 export default ({
   login,
   handleToggle,
-}) => (
-  <div>
-  <label>Username</label>
-  <input type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)} required />
-  <label>Password</label>
-  <input type="text" name="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-  <button type="submit" onClick={() => login(username, password)}>Login here!</button>
-  <button type="button" onClick={handleToggle}>Create Account Here!</button>
-  </div>
-);
+}) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  return (
+    <div>
+    <label>Username</label>
+    <input type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)} required />
+    <label>Password</label>
+    <input type="text" name="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+    <button type="submit" onClick={() => login(username, password)}>Login here!</button>
+    <button type="button" onClick={handleToggle}>Create Account Here!</button>
+    </div>
+  );
+};
